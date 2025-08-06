d64bf96965ee11ebc6b3de1a99a729c696c1ee6c
Prajwal Malokar <malokarprajwal4@gmail.com>
2025-08-07 00:16:00 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+
+    if __name__ == '__main__':
+        app.run(debug=True, host='0.0.0.0', port = 5000)
+
+    return app
+
+
+
---
+++ test_app.py
@@ -0,0 +1,18 @@
+import pytest
+from app import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    yield app
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"<title>My-Flask-App</title>" in response.data
+
+
